57fe743b07e9c7327feada81fa31d263b7c3de5d
Kseniya <pashkina.ks@mail.ru>
2022-09-28 16:55:29 +0700

    задача_53

---
+++ Задача_53/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int height = EnterInt("Enter height: ");
+int width = EnterInt("Enter width: ");
+
+int[,] numbers = new int[height, width];
+Fill2DArray(numbers, height, width);
+Print2DArray(numbers, height, width);
+Console.WriteLine();
+ChangeFirstEndRows(numbers, height, width);
+Print2DArray(numbers, height, width);
+
+int EnterInt(string prompt)
+{
+    Console.Write(prompt);
+    return int.Parse(Console.ReadLine()!);
+}
+
+
+void Fill2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            numbers[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+
+void Print2DArray(int[,] numbers, int height, int width)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write($"{numbers[i, j], 2} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ChangeFirstEndRows(int[,] numbers, int height, int width)
+{
+    for (int j = 0; j < width; j++)
+    {
+        (numbers[0, j], numbers[height - 1, j]) = (numbers[height - 1, j], numbers[0, j]);
+    } 
+}
